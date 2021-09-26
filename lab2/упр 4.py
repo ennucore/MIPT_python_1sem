14a466efa530996eb8983058d7305b56544dfce2
RufovD <91056290+RufovD@users.noreply.github.com>
2021-09-26 22:17:55 +0300

    Delete lab2 directory

--- lab2/упр 4.py
+++
@@ -1,7 +0,0 @@
-import turtle
-
-turtle.shape('turtle')
-
-for i in range(0, 371, 1):
-    turtle.forward(2)
-    turtle.left(1)
--- lab2/упр 7.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-turtle.shape('turtle')
-
-z=0.1
-for i in range(0, 200):
-    turtle.forward(z)
-    turtle.left(10)
-    z=z+0.1
